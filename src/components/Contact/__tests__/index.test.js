e450568607178f5cdeb31bc0587807936acf9b5b
Jito Chadha <jitochadha@gmail.com>
2021-05-03 14:48:43 -0700

    contact tests updated

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Contact from "src/components/Contact/index.js";
+
+afterEach(cleanup);
+
+describe("Contact component renders", () => {
+  it("renders", () => {
+    render(<Contact />);
+  });
+
+  it("matches snapshot", () => {
+    const { asFragment } = render(<Contact />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+it("renders", () => {
+  const { getByTestId } = render(<Contact />);
+
+  expect(getByTestId("h1tag")).toHaveTextContent("Contact me");
+});
+
+it("renders", () => {
+  const { getByTestId } = render(<Contact />);
+
+  expect(getByTestId("button")).toHaveTextContent("Submit");
+});
